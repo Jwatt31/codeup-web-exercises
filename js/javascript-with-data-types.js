74364c0d9048a01fd68478712a09e8b2436929ed
James Whatley <jamesww189@gmail.com>
2023-10-12 16:13:14 -0500

    notes and Html js connection class exersies

---
+++ js/javascript-with-data-types.js
@@ -0,0 +1,27 @@
+"use strict"
+
+let myAge = 5
+console.log(myAge);
+
+//alert
+alert("hello lasso");
+
+//confirm
+let confirmed =confirm  ("Are you sure your in the correct cohort");
+console.log(confirmed);
+
+//prompt
+let userinput =prompt("please type in your name");
+alert(`great to meet you ${userinput}`);
+
+
+console.log("this is my external js fail");
+
+
+
+        //concatanation vs templet lit ex
+    let myName = "james";
+    let myAge = 30;
+
+
+
