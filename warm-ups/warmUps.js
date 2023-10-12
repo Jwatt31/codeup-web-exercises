6aef0cdda3f4df937a711f8254002f9c4726b740
James Whatley <jamesww189@gmail.com>
2023-10-12 09:18:27 -0500

    adding in alt command in warm ups

--- warm-ups/warmUps.js
+++ warm-ups/warmUps.js
@@ -2,4 +2,5 @@
 // "Daily Warm-up
 let name = "James Whatley"
 let year = "2011"
-console.log("my name is " + name +" I graduated high School in " + year)
+console.log("my name is " + name +" I graduated high School in " + year);
+//console.log(`my name is ${name} and I graduated high school in ${year}`); (alt)
