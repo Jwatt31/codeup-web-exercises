1ddb3d5bdcfa4f0b801f3447731ec61063dff2c0
James Whatley <jamesww189@gmail.com>
2023-10-16 10:19:44 -0500

    warm up 0ct 16th notes and completion

--- warm-ups/warmUps.js
+++ warm-ups/warmUps.js
@@ -4,3 +4,12 @@ let name = "James Whatley"
 let year = "2011"
 console.log("my name is " + name +" I graduated high School in " + year);
 //console.log(`my name is ${name} and I graduated high school in ${year}`); (alt)
+
+
+const capitalizeFirstLetter = (str) => str.substring(0,1).toUpperCase() + str.substring(1);
+//// this takes out the first letter changes its value thrn adds it back in
+
+////replace method is another way the issue come in when your dont know the word being written in
+//const
+
+console.log(capitalizeFirstLetter("hello world"))
