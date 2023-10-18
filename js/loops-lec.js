244e542ced60e08ceea8456e6c41e26c3c2e33a8
James Whatley <jamesww189@gmail.com>
2023-10-18 14:42:35 -0500

    saving progress

--- js/loops-lec.js
+++ js/loops-lec.js
@@ -26,11 +26,41 @@
 let num = 0
 
 // while (num < 100){
-//     console.log(num)
+//     console.log(num);
 //     num += 5
 // }
 
+// while (num < 100){
+//     console.log(num = num + 5);
+// } // will end at 100 unlike the above
+
+// do {
+//     console.log(num);
+//     num += 5
+// } while (num < 100);
+
+// do {
+//     console.log(num = num + 5)
+// } while (num < 100)
+///// this one still ends on 100 unlike above
+
+const secretNumber =7
+let guess;
+let attempt = 0
+//do while loop
+
 do {
-    console.log(num);
-    num += 5
-} while (num < 100)
+    guess = parseInt(prompt("guess the secret number (1-10):"));
+    attempt++
+    if (guess === secretNumber){
+        console.log("you got it soup for you")
+    } else {
+        if (guess < secretNumber){
+            console.log("A little low.")
+        } else {
+            console.log("just like these prices its too high")
+        }
+        console.log(`guess again or no soup for you. after ${attempt} many trys you would think you would get it `)
+    }
+
+}while (guess !== secretNumber)
