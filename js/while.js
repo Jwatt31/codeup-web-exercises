66bbb126904ec6b401e262fa45789a454c3bc605
James Whatley <jamesww189@gmail.com>
2023-10-18 16:34:43 -0500

    notes for the while.js

--- js/while.js
+++ js/while.js
@@ -1,19 +1,19 @@
-// let num = 1
-//
-// while (num < 65536 ){
-//     console.log(num = num * 2)
-// }
+let num = 1
+
+while (num < 65536 ){
+    console.log(num = num * 2)
+}
 
 let allCones = Math.floor(Math.random() * 50) + 50;
 let numSold;
 
 do {
-     numSold = Math.floor(Math.random() * 5) + 1;
-    if (allCones < numSold ){
+     numSold = Math.floor(Math.random() * 5) + 1; /// this gives us a random number
+    if (allCones < numSold ){ /// this is saying that as longg as all clones sold are less than number sold at that iterration of sale the statment is false once it becomes true it gives us the log
         console.log(`sorry im out i can do `+ numSold + ' i have ' + allCones );
-        continue;
+        continue; //this is acting as a repetative telling the above to repeat until its true or if you have a set number or input that becomes false and is
     }
-    allCones -= numSold
+    allCones -= numSold //
     console.log(numSold + ' cones sold')
 } while (allCones > 0)
 
