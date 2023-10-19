0b1c43eebdd4d951ba801ee8e2f41ff3490d48ad
James Whatley <jamesww189@gmail.com>
2023-10-19 09:27:20 -0500

    warm up oct 19

--- warm-ups/warmUps.js
+++ warm-ups/warmUps.js
@@ -1,28 +1,40 @@
-// James Whatley
-// "Daily Warm-up
-let name = "James Whatley"
-let year = "2011"
-console.log("my name is " + name +" I graduated high School in " + year);
-//console.log(`my name is ${name} and I graduated high school in ${year}`); (alt)
-
-
-const capitalizeFirstLetter = (str) => str.substring(0,1).toUpperCase() + str.substring(1);
+// // James Whatley
+// // "Daily Warm-up
+// let name = "James Whatley"
+// let year = "2011"
+// console.log("my name is " + name +" I graduated high School in " + year);
+// //console.log(`my name is ${name} and I graduated high school in ${year}`); (alt)
+//
+//
+// const capitalizeFirstLetter = (str) => str.substring(0,1).toUpperCase() + str.substring(1);
 //// this takes out the first letter changes its value thrn adds it back in
 
 ////replace method is another way the issue come in when your dont know the word being written in
 //const
 
-console.log(capitalizeFirstLetter("hello world"))
+//console.log(capitalizeFirstLetter("hello world"))
 
 /////oct 17 warm up find the heighest number
 
-function largestNum (in1, in2, in3){
-    return Math.max (in1, in2, in3)
-}
-
-console.log(largestNum(5,10,15))
+// function largestNum (in1, in2, in3){
+//     return Math.max (in1, in2, in3)
+// }
+//
+// console.log(largestNum(5,10,15))
 // function largestnum (){
 // let num1 = parsenInt(prompt('') /// repeat as needed
 // let largestNum = math.max
 // alart(
 // largestNum()
+
+/////// warmup oct 19
+let num = 100
+while (num -= 5) {
+    if (num % 2 === 0) {
+        console.log(`${num} even`)
+    } else console.log(num)
+}
+
+
+
+
