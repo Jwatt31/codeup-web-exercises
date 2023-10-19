0bc4a9471fc9d8d0ab5e644116a00c986fe019f2
James Whatley <jamesww189@gmail.com>
2023-10-19 14:42:24 -0500

    done with loop work

--- js/for_loops.js
+++ js/for_loops.js
@@ -3,13 +3,13 @@
 ////// loops ex 2
 function showMultiplicationTable(input){
     for (let i = 0; i <=10; i++){
-        console.log(input + "x" + i + "=" + i * input )
+        console.log(input + " x " + i + " = " + i * input )
     }
 }
 showMultiplicationTable(7)
 
 ///////loops ex 3
-let ranNum = Math.floor(Math.random() * 180) + 20
+//let ranNum = Math.floor(Math.random() * 180) + 20
 for (let j =0 ; j < 10 ; j++ ){
     let ranNum = Math.floor(Math.random() * 180) + 20
     if (ranNum % 2 === 0){
@@ -19,3 +19,13 @@ for (let j =0 ; j < 10 ; j++ ){
     }
 }
 
+/////////////////--------------- 4 ex
+
+for(let i = 1; i < 10; i++ ){
+    console.log(i.toString().repeat(i))
+}
+
+////////////////----- 5 ex
+for (let i = 100; i > 0; i -= 5){
+    console.log(i)
+}
