f0ae4da4c10980f7b4bf1fcfbfca2b1a85e9fd5c
James Whatley <jamesww189@gmail.com>
2023-10-27 16:23:42 -0500

    Brute force it

---
+++ Js_extrapractice/Html_elements.js
@@ -0,0 +1,31 @@
+'use strict'
+
+function heading(string) {
+    let heading1 = '<h1>';
+    heading1 += string + '</h1>';
+    return heading1
+}
+
+console.log(heading('how i met your mothers'))
+
+function noOrder(item, item2, item3) {
+    let top = '<ul>' +"\n";
+    let first = top += "     "+ '<li>' + `${item2}`+`</li>`
+    let second = "     "+ "<li>" + `${item}` + '</li>'
+    let third= "     "+'<li>' + item3 + '</li>'
+    return first + "\n" + second + "\n" + third +"\n" +'</ul>'
+}
+console.log(noOrder('cat','dog','snake'))
+
+
+
+function Order(str) {
+    let newOrder = str.split(",")
+    let orderList = '<ul>'
+    for (let i = 0; i < Order.length; i++) {
+
+    }
+
+}
+
+console.log(Order(['cat','dog','snake']))
