9766fee61ef3fdadee5821d2496c0253f243bae4
James Whatley <jamesww189@gmail.com>
2023-11-16 14:29:33 -0600

    going over parks html

--- js/dom-traversal-lec.html.js
+++ js/dom-traversal-lec.html.js
@@ -1,5 +1,5 @@
 const box1=document.querySelector('.box1');
-const parent = box1.parentElement
+const parent = box1.parentElement/*if u chain these u can go all the way to the body*/
 console.log(parent)
 
 /*3 things that make an elemet
@@ -13,11 +13,23 @@ console.log(parent)
 //     stateParksLIs[i].style.fontWeight = "bold";
 // }
 const stateParksUL = document.getElementById('state-parks-texas');
-stateParksUL.parentElement.style.backgroundColor='lightblue';
 
+stateParksUL.parentElement.style.backgroundColor='lightblue';
+/* ---------------------------------------------------------- */
 const stateParksLis=document.querySelector('#state-parks-texas').children;
+
 for (let i = 0; i < stateParksLis.length; i++) { stateParksLis[i].style.color='red'
     stateParksLis[2].style.fontWeight='bold'
     stateParksLis[4].style.fontSize='30px'
 
 }
+/*----------------------------------------------------------------------------------*/
+const nationalParksUL =document.querySelector('#national-parks')
+
+nationalParksUL.firstElementChild.style.color="green";
+
+nationalParksUL.lastElementChild.style.backgroundColor='beige';
+
+/*-----------------------------------------------------------------*/
+const nationalParksHeading= document.querySelector('#national-parks-heading');
+nationalParksHeading.nextElementSibling.style.backgroundColor='salmon'
