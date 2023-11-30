38e3726db2693f438a275882a5b2f56579551f1f
James Whatley <jamesww189@gmail.com>
2023-11-30 14:53:52 -0600

    saving before i crash

--- js/Weather_map.js
+++ js/Weather_map.js
@@ -5,6 +5,7 @@ mapboxgl.accessToken = MAPBOX_API;
 
 
 /*Geocode */
+
 function geocode(search, token) {
     var baseUrl = 'https://api.mapbox.com';
     var endPoint = '/geocoding/v5/mapbox.places/';
@@ -13,30 +14,58 @@ function geocode(search, token) {
         .then( data => data.features[0].center);
 }
 
-function reverseGeocode(coordinates, token) {
-    var baseUrl = 'https://api.mapbox.com';
-    var endPoint = '/geocoding/v5/mapbox.places/';
-    return fetch(`${baseUrl}${endPoint}${coordinates.lng},${coordinates.lat}.json?access_token=${token}`)
-        .then( res => res.json() )
-        .then( data => data.features[0].place_name );
-}
-
 /*weather map fetch and map*/
 
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/navigation-night-v1',
+    style: 'mapbox://styles/mapbox/outdoors-v12',
     zoom: 10,
-    center: [-96.80315647882033, 32.777560104836496]
+    center: [-97.31636650243732, 32.75433667827946]
 })
 
 fetch(`https://api.openweathermap.org/data/2.5/forecast?` +
-    `lat=29.426825118534886&lon=-98.48948239256946` +
+    `lat=32.75433667827946&lon=-97.31636650243732` +
     `&appid=${OPEN_WEATHER_API}`+`&units=imperial`)
     .then(data => data.json())
     .then(result => {const date = new Date(result.dt * 1000);
         console.log(date.toLocaleDateString())
         console.log(result)
+
     });
 
+
+/*setting location */
+
+document.getElementById("sub").addEventListener("click", function () { /*grab the button*/
+    let Currentloca = geocode(document.getElementById("search").value, MAPBOX_API);
+    Currentloca.then(result => {
+        console.log(result)
+        map.setCenter([result[0], result[1]])/*calll back to the map above in this code*/
+    })
+    Currentloca.then()
+})
+
 /*Dark mode add in */
+
+// function switchToDarkModeIfNeeded() {
+//     let currentDate = new Date();
+//     let currentHour = currentDate.getHours();
+//     if (currentHour >= 14) {
+//         document.body.classList.add ('dark-mode');
+//     }else {
+//         document.body.classList.remove('dark-mode');
+//     }
+// }
+//
+// setInterval(switchToDarkModeIfNeeded, 6000);
+
+// setTimeout()
+/*location in header*/
+let currentLocation = document.querySelector('.currentLocation')
+currentLocation.innerText(`${geocode(document.getElementById("search").value)}`)
+
+
+
+
+
+
