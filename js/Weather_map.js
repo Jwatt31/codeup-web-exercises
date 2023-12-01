52b2aa39530dc278413d776be0342034f898ca84
James Whatley <jamesww189@gmail.com>
2023-11-30 22:07:09 -0600

    tried to fix issues with the staking cards

--- js/Weather_map.js
+++ js/Weather_map.js
@@ -31,11 +31,13 @@ fetch(`https://api.openweathermap.org/data/2.5/forecast?` +
         const date = new Date(result.dt * 1000);
         console.log(date.toLocaleDateString())
         console.log(result)
+        /*add in appends so these appear and disapper when search is made*/
 
     });
 
 
 /*setting location */
+
 
 // const location = document.getElementById("search").value;
 document.getElementById("sub").addEventListener("click", function () { /*grab the button*/
@@ -54,14 +56,17 @@ document.getElementById("sub").addEventListener("click", function () { /*grab th
                     console.log(data)
 
                     const day = data.list
-                    for (let i = 0; i < 40; i += 8 ) {
+                    for (let i = 0; i < 40; i += 8 ) {/*40 because thats how many 3 hour cycals there are in a 3 day */
                         const weather = day[i]
 
                         const  date =new Date(weather.dt * 1000);
                         // console.log(date.toLocaleDateString()
+                        // ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
+                        // when asked for a state you get overlapping cards if time input an if statment to limet the cards that appear
 
 
-                        const DataDiv =document.createElement('div')
+                        const DataDiv = document.createElement('div');
+
                         const time = document.createElement('h2')
                         const temp = document.createElement('p')
                         const Icon = document.createElement('img')
@@ -88,13 +93,24 @@ document.getElementById("sub").addEventListener("click", function () { /*grab th
                         DataDiv.appendChild(pressuer)
 
                         DataDiv.classList.add('Weather-Data')
+                        if (DataDiv.length){
+                            DataDiv[0].parentNode.removeChild()
+                        }
+
+
+                        /*need to add a remove function*/
+
+
 
                     }
 
 
                 })
         })
+
 })
+
+
 /*Dark mode add in */
 
 // function switchToDarkModeIfNeeded() {
